f2c28c5846d9af59c88c809258cfcc017465ed42
Danilo Keiiti <dkeiiti@gmail.com>
2021-12-05 18:03:51 -0300

    Changes in the website message

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = ()=>{
   return (
     <div className="App">
       <header className="App-header" >
-        <h1>Welcome to CarIOTA simulator</h1>
+        <h1>Welcome to CarIOTA</h1>
         <p>Soon you'll drive a virtual connected car empowered by IOTA</p>
         <img className="App-logo" src="/images/iota_logo.png" alt="" />
       </header>
