9a0206d042e7d2a805613ee062dcb16cddf97e70
dkeiiti <30915922+dkeiiti@users.noreply.github.com>
2022-03-02 11:03:03 -0300

    Update nutshell.tsx
    Improvement in text

--- src/components/home/nutshell.tsx
+++ src/components/home/nutshell.tsx
@@ -12,7 +12,7 @@ const AppOurGoal = () => {
                     <div className="line-43B9AA "/>
                     <div className="nutshell-text">
                         <Text style={{color: '555'}}>Our goal is to bring the machine-to-machine (M2M) economy to life.</Text>
-                        <Text style={{color: '555'}}>We develop code and games aiming industrial applications. We undestard it's a long road to an ideia from proof of concept till production-readiness. There are plenty of technical and business models testing, plant pilots, internal 6 external validation, etc. to be overcome till industrial scalability can be reached. And that's why we focus on building M2M gaming  plataforms powered by IOTA - to help industries to overcome those needs by providing an interactive testbed.</Text>
+                        <Text style={{color: '555'}}>We provide a sandbox where business models can be tested and improved. We understand it's a long way from an ideia to proof of concept, then to production-readiness. There are plenty of technical issues, regulation compliance, stakeholder validation, etc. to be overcome till market launch and scalability can be reached.</Text>
                         <img src="./assets/images/CarNutshell.svg" />
                         <Text style={{color: '555'}}>...Picture machines in a production line communicating with suppliers, paying for maintenance services and updates while selling their operational data.</Text>
                     </div>
